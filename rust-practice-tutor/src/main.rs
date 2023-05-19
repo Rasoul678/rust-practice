7d4f7b1f9e4a89ae8fedc6f3d1f6292d652811b2
Rasoul Hesami Rostami <h.rostami.r@gmail.com>
2023-05-19 20:23:21 +0330

    Functions

--- rust-practice-tutor/src/main.rs
+++ rust-practice-tutor/src/main.rs
@@ -1,11 +1,14 @@
 fn main() {
     parts::one();
     parts::two();
+    parts::three();
 }
 
 mod parts {
     pub mod ownership;
     pub mod variables;
+    pub mod functions;
     pub use ownership::show as two;
     pub use variables::show as one;
+    pub use functions::show as three;
 }
---
+++ rust-practice-tutor/src/parts/functions.rs
@@ -0,0 +1,51 @@
+pub fn show() {
+    println!("################# functions #################");
+    let message = get_hello_world();
+    // println!("{}", message);
+    say_hello_world(&message);
+    let hello = say_hello_to(String::from("Rasoul"));
+    println!("{}", hello);
+
+    // Inline function
+    let inline_func = |name: &str| format!("Hey {}", name);
+    let msg = inline_func(&message);
+    println!("{}", msg);
+
+    let ask_for_age = || {
+        let mut age = String::new();
+        println!("How old are you?");
+        std::io::stdin()
+            .read_line(&mut age)
+            .expect("Failed to read line");
+        age.trim().parse::<u32>().expect("Please type a number: ");
+        age
+    };
+
+    let age = ask_for_age();
+    println!("You are {} years old!", age);
+
+    // Function pointer
+    let multiply_by_2 = |x: i32| x * 2;
+    let ptr = multiply_by_2;
+    println!("{}", ptr(2));
+
+    // Function as argument
+    process_name(&message, say_hello_world);
+    println!("################# functions #################");
+}
+
+fn process_name(name: &str, callback: fn(&str) -> ()) {
+    callback(name);
+}
+
+fn get_hello_world() -> String {
+    String::from("Hello World!")
+}
+
+fn say_hello_world(message: &str) {
+    println!("{}", message);
+}
+
+fn say_hello_to(to_person: String) -> String {
+    format!("Hello {}!", to_person)
+}
