51a5ff2e8d7501df7813882ff92980f61e6c46f3
Rasoul Hesami Rostami <h.rostami.r@gmail.com>
2023-06-09 18:58:11 +0330

    :sparkles: lib

---
+++ myapp/src/main.rs
@@ -0,0 +1,10 @@
+use intutils::addition::add;
+use intutils::subtraction::sub;
+
+fn main() {
+    let added = add(1, 2);
+    println!("{}", added);
+
+    let subtracted = sub(5, 1);
+    println!("{}", subtracted);
+}
